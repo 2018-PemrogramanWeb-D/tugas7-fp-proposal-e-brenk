da2b37483102af5edcd6a8309bc2ca8314528f83
azcrea <36807423+azcrea@users.noreply.github.com>
2018-11-28 11:33:02 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = “localhost”;
-$username = “Gusrai”;
-$password = “jonmin123”;
-$database = “e-brenk”;
-mysql_connect($server,$username,$password) or die(“Koneksi gagal”
-mysql_select_db($database) or die(“Database tidak bisa dibuka”
-?>
